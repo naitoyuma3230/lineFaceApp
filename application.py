bd3b0b26fde55f2da8ed02bed7391dde7d5f79d0
naitoyuma3230 <naitoyuma@outlook.jp>
2022-06-08 13:06:48 +0000

    顔識別の結果を返答

--- application.py
+++ application.py
@@ -32,6 +32,9 @@ app = Flask(__name__)
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
 YOUR_CHANNEL_SECRET = os.getenv('YOUR_CHANNEL_SECRET')
+PERSON_GROUP_ID = os.getenv('PERSON_GROUP_ID')
+PERSON_ID_AUDREY = os.getenv('PERSON_ID_AUDREY')
+
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -79,10 +82,27 @@ def handle_image(event):
         # 検出結果に応じて処理を分ける
         if detected_faces != []:
             # 検出された顔の最初のIDを取得
-            text = detected_faces[0].face_id
+            # text = detected_faces[0].face_id
+
+            # 顔検出ができたら顔認証を行う
+            valified = face_client.face.verify_face_to_person(
+                face_id = detected_faces[0].face_id,
+                person_group_id = PERSON_GROUP_ID,
+                person_id = PERSON_ID_AUDREY
+            )
+            # 認証結果に応じて処理を変える
+            if valified:
+                if valified.is_identical:
+                    # 顔認証が一致した場合（スコアもつけて返す）
+                    text = 'この写真はオードリーヘップバーンです(score:{:.3f})'.format(valified.confidence)
+                else:
+                    # 顔認証が一致した場合（スコアもつけて返す）
+                    text = 'この写真はオードリーヘップバーンではありません(score:{:.3f})'.format(valified.confidence)
+            else:
+                text = '識別できませんでした。'
         else:
             # 検出されない場合のメッセージ
-            text = "no faces detected"
+            text = "写真から顔が検出できませんでした。他の画像で試してください。"
     except:
         # エラー時のメッセージ
         text = "error" 
